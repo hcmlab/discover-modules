a3353545a12ce1f408af43f075e0591bb9411fa5
Dominik Schiller <dominik.schiller@me.com>
2024-11-22 20:59:53 +0100

    Update lens_free_prompt

--- modules/lens/lens_free_prompt.py
+++ modules/lens/lens_free_prompt.py
@@ -6,7 +6,9 @@ from discover_utils.utils.log_utils import log
 import json
 
 
-_default_options = {}
+_default_options = {
+    'group_turns': False,
+}
 _dim_labels = [
     "sentiment",
 ]
@@ -25,24 +27,25 @@ class LensFreePrompt(Processor):
         self.port = str(self.options['port'])
         self.model = self.options['model']
         self.provider = self.options['provider']
-        self.system_prompt = self.options.get('system_prompt')
-        self.prompt = self.options.get('prompt')
+        self.prompt = self.options.get('prompt', '')
+        self.group_turns = self.options.get('group_turns', False)
 
     def preprocess_sample(self, sample):
         return sample
 
-    def process_sample(self, sample, system_prompt):
+    def predict_sample(self, sample, prompt):
         request = {
-            'system_prompt': system_prompt,
+            'system_prompt': '',
             'provider': self.provider,
             'model': self.model,
-            'message': f'Predict the following data: <{sample}>. Value:\n',
+            'message': f'{prompt}. \n """{sample}""". Value:\n',
             'temperature': 0,
             #'top_k': 1,
             #"top_p" : 0.01
             'resp_format' : 'json',
-            'max_new_tokens': 1024,
-            'enforce_determinism': True
+            'max_new_tokens': 128,
+            'enforce_determinism': True,
+            'stream': True
         }
         with requests.post('http://' + self.ip + ':' + self.port + '/assist', json=request) as response:
             resp = response.content
@@ -50,7 +53,7 @@ class LensFreePrompt(Processor):
         try:
             resp = json.loads(resp.decode('UTF-8'))
         except Exception as e:
-            log (e)
+            log(str(e))
             resp = {}
 
         return resp
@@ -59,54 +62,24 @@ class LensFreePrompt(Processor):
         self.session_manager = self.get_session_manager(ds_manager, ignore_ambiguity=True)
 
         odt = self.session_manager.output_data_templates[OUTPUT_ID]
-        if isinstance(odt, DiscreteAnnotation):
-            annotation_scheme_name = odt.annotation_scheme.name
-            class_ids = odt.annotation_scheme.classes
-            annotation_scheme_description = odt.meta_data.description
-            annotation_scheme_examples = odt.meta_data.examples
-
-
-            response_schema_json = """Respond in JSON. Your response should be in the following format:
-                ```json
-                {
-                  "label": "The label you assigned to the text segment.",
-                  "confidence": "A float value on a continuous scale from 0.0 to 1.0 that indicates how confident you are in your prediction. 0.0 means your are just guessing and 1.0 means you are absolut certain.",
-                  "explanation": "Explain why you assigned the label to the input."
-                }
-                ```
-            """
-
-            sp = f'You are a classifier that assigns labels to text segments.' \
-                 f'You label every segment with respect to {annotation_scheme_name}. ' \
-                 f'To assign the label chose one of the following categories:{", ".join([x["name"] for x in class_ids.values()])}.' \
-
-            if annotation_scheme_description:
-                 sp += f'Follow the following description of the labels:\n{annotation_scheme_description}'
-            if annotation_scheme_examples:
-                ex = "\n".join([x["value"] + " : " +  x["label"] for x in annotation_scheme_examples])
-                sp += f'Use the following examples as a guideline for your labeling process:\n { ex }'
-
-            sp += f'{response_schema_json}'
-        else:
-            sp = self.system_prompt + f'Respond in JSON. Only use one key called <label>.'
-
+        if not self.prompt.endswith('.'):
+            self.prompt = self.prompt + '.'
+        prompt = self.prompt + f' Respond in JSON. Only use one key called "label".'
         data_object = self.session_manager.input_data[INPUT_ID]
-        data_object_context = self.session_manager.input_data[INPUT_ID_CONTEXT]
-
         data = data_object.data
-        data_context = data_object_context.data
 
         ret = []
         attributes = {
             a : [] for a in ATTRIBUTES
         }
 
-        # TODO MAKE OPTION
-        group_turns = True
-        if group_turns:
+        if self.group_turns:
+            data_object_context = self.session_manager.input_data[INPUT_ID_CONTEXT]
+            data_context = data_object_context.data
+
             import pandas as pd
-            transcript = pd.core.frame.DataFrame(data)
-            context = pd.core.frame.DataFrame(data_context)
+            transcript = pd.DataFrame(data)
+            context = pd.DataFrame(data_context)
             transcript['id']  = data_object.meta_data.role
             context['id']  = data_object_context.meta_data.role
             df = pd.concat([transcript, context])
@@ -151,30 +124,18 @@ class LensFreePrompt(Processor):
             data = df.to_numpy()
 
         for i,d in enumerate(data):
-            resp = self.process_sample(d[2], system_prompt=sp)
+            resp = self.predict_sample(d[2], prompt=prompt)
             print(resp)
+            resp = resp.get('label', UNK)
+            ret.append((d[0], d[1], resp, d[3]))
 
-            if isinstance(odt, DiscreteAnnotation):
-                label_candidates = [k for k,v in odt.annotation_scheme.classes.items() if v['name'] == resp.get('label')]
-                if label_candidates:
-                    label_id = label_candidates[0]
-                else:
-                    label_id = odt.rest_label_id
-                label_id = int(label_id)
-                conf = float(resp.get('confidence', 0.0))
-                ret.append((d[0], d[1], label_id, conf))
-            else:
-                resp = resp.get('label', UNK)
-                ret.append((d[0], d[1], resp, d[3]))
-
-            for k,v in attributes.items():
-                v.append(resp.get(k.lower(), ''))
-        return ret, attributes
+        return ret
 
     def to_output(self, data) -> dict:
         # Append necessary meta information
         out = self.session_manager.output_data_templates[OUTPUT_ID]
-        out.data, out.meta_data.attributes = data
+        #out.data, out.meta_data.attributes = data
+        out.data = data
         return self.session_manager.output_data_templates
 
 if __name__ == '__main__':
@@ -186,41 +147,26 @@ if __name__ == '__main__':
     from discover_utils.utils.ssi_xml_utils import Trainer
 
 
-    env = dotenv.load_dotenv(r'../.env')
+    env = dotenv.load_dotenv(r'../../.env')
     IP = os.getenv("NOVA_IP", "")
     PORT = int(os.getenv("NOVA_PORT", 0))
     USER = os.getenv("NOVA_USER", "")
     PASSWORD = os.getenv("NOVA_PASSWORD", "")
     DATA_DIR = Path(os.getenv("NOVA_DATA_DIR", None))
-    ASSISTANT_IP = os.getenv("NOVA_ASSISTANT_IP", "")
-    ASSISTANT_PORT = os.getenv("NOVA_ASSISTANT_PORT", 0)
-
-
-
-# amh = AnnotationHandler(db_host=IP, db_port=PORT, db_user=USER, db_password=PASSWORD)
-
-    # load
-    #fs = "Loading {} took {}ms"
-    #t_start = perf_counter()
-    #discrete_anno = amh.load(
-    #     dataset="test",
-    #     scheme="emotion_categorical",
-    #     annotator="schildom",
-    #     session="01_AffWild2_video1",
-    #     role="testrole2",
-    #     header_only=True
-    # )
-    #t_stop = perf_counter()
-    #print(fs.format("Discrete annotation", int((t_stop - t_start) * 1000)))
+    LENS_IP = os.getenv("LENS_IP", "")
+    LENS_PORT = os.getenv("LENS_PORT", 0)
+    DISCOVER_OUT_DIR = Path(os.getenv("DISCOVER_OUT_DIR", None))
 
     # Trainer
     na_trainer = Trainer()
-    na_trainer.load_from_file("nova_assistant_predict.trainer")
+    na_trainer.load_from_file("lens_predict.trainer")
     opts = {
-        'ip': ASSISTANT_IP,
-        'port': ASSISTANT_PORT,
-        'model': 'llama2',
-        'provider': 'ollama'
+        'ip': LENS_IP,
+        'port': LENS_PORT,
+        'model': 'mistral-nemo',
+        'provider': 'ollama',
+        'prompt': 'translate the provided text to english',
+        'group_turns': True,
     }
     na = LensFreePrompt(model_io=None, opts=opts, trainer=na_trainer)
 
@@ -228,21 +174,28 @@ if __name__ == '__main__':
     transcript = {
         "src": "db:annotation:Free",
         "scheme": "transcript",
-        "role": "testrole2",
-        "annotator": "baurtobi",
+        "role": "patient",
+        "annotator": "whisperx_segment",
         "type": "input",
         "id": INPUT_ID,
-
     }
 
+    transcript_ctx = {
+        "src": "db:annotation:Free",
+        "scheme": "transcript",
+        "role": "therapeut",
+        "annotator": "whisperx_segment",
+        "type": "input",
+        "id": INPUT_ID_CONTEXT
+    }
+
+
     # Output
-    sentiment = {
-        "src": "db:annotation:Discrete",
-        "scheme": "emotion_categorical",
-        "role": "testrole2",
-        "annotator": "baurtobi",
+    translation = {
+        "src": "file:annotation:Free",
         "type": "output",
         "id": OUTPUT_ID,
+        "uri": str(DISCOVER_OUT_DIR / 'Lens' / 'lens_predict.py.annotation'),
     }
 
     ctx = {
@@ -257,15 +210,17 @@ if __name__ == '__main__':
 
 
     ds_iterator = DatasetIterator(
-        data_description=[transcript, sentiment],
+        data_description=[transcript, transcript_ctx, translation],
         source_context=ctx,
-        dataset='test',
+        dataset='therapai',
         frame_size=na_trainer.meta_frame_step,
         left_context=na_trainer.meta_left_ctx,
-        session_names=['01_AffWild2_video1']
+        session_names=['88Y8_S03']
     )
     ds_iterator.load()
     emotions = na.process_data(ds_iterator)
     output = na.to_output(emotions)
+
+    breakpoint()
 
 
