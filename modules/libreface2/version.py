561a85aeccdeff45b314065f23b022947e705243
saveli <abhaltenimlos@gmail.com>
2025-02-20 17:23:06 +0100

    imagewise working init

---
+++ modules/libreface2/version.py
@@ -0,0 +1,18 @@
+""" LibreFace
+
+Author:
+    Tobias Hallmen <tobias.hallmen@uni-a.de>
+Date:
+    20.02.2025
+
+"""
+# We follow Semantic Versioning (https://semver.org/)
+_MAJOR_VERSION = '1'
+_MINOR_VERSION = '1'
+_PATCH_VERSION = '0'
+
+__version__ = '.'.join([
+    _MAJOR_VERSION,
+    _MINOR_VERSION,
+    _PATCH_VERSION,
+])
